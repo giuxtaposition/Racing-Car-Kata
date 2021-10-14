3f29d4dd2a639a41910ee0af6f6fc88988d6b054
giuxtaposition <yg97.cs@gmail.com>
2021-10-14 17:41:39 +0200

    added more tests for ticket-dispenser

--- turn-ticket-dispenser-tests/ticket-dispenser-test.ts
+++ turn-ticket-dispenser-tests/ticket-dispenser-test.ts
@@ -4,7 +4,9 @@ import TicketDispenser from '../turn-ticket-dispenser/ticket-dispenser'
 import TurnNumberSequence from '../turn-ticket-dispenser/turn-number-sequence'
 import { instance, mock, when } from 'ts-mockito'
 
-// - [ ] Ask for ticket, receive ticket
+// - [x] Ask for ticket, receive ticket
+// - [ ] New Ticked number is greater than last ticket number
+// - [ ] New Ticket number is greater than last ticket number even when using two ticket dispensers
 
 describe('Turn Ticket Dispenser', () => {
     it('Ask for ticket, receive ticket with my number', () => {
@@ -18,5 +20,30 @@ describe('Turn Ticket Dispenser', () => {
             instance(mockedTurnNumberSequence)
         )
         expect(ticket.getTurnNumber()).to.equal(13)
+    })
+
+    it('New ticket number is greater than previous ticket number', () => {
+        const turnNumberSequence: TurnNumberSequence = new TurnNumberSequence()
+        const dispenser = new TicketDispenser()
+
+        const previousTicket = dispenser.getTurnTicket(turnNumberSequence)
+        const newTicket = dispenser.getTurnTicket(turnNumberSequence)
+
+        expect(newTicket.getTurnNumber()).to.greaterThan(
+            previousTicket.getTurnNumber()
+        )
+    })
+
+    it('Using two dispenser: new ticket number is still greater than previous ticket number', () => {
+        const turnNumberSequence: TurnNumberSequence = new TurnNumberSequence()
+        const dispenser = new TicketDispenser()
+
+        const previousTicket = dispenser.getTurnTicket(turnNumberSequence)
+        const otherDispenser = new TicketDispenser()
+        const newTicket = otherDispenser.getTurnTicket(turnNumberSequence)
+
+        expect(newTicket.getTurnNumber()).to.greaterThan(
+            previousTicket.getTurnNumber()
+        )
     })
 })
