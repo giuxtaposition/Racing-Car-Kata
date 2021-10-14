5051c9715063eff52980bf65c885e5320d99bb34
giuxtaposition <yg97.cs@gmail.com>
2021-10-14 17:14:44 +0200

    added test for ticket-dispenser

--- .mocharc.js
+++ .mocharc.js
@@ -4,5 +4,5 @@ module.exports = {
     require: ['ts-node/register', 'source-map-support/register'],
     recursive: true,
     // spec: '*/*-test.ts',
-    spec: '*/alarm-test.ts',
+    spec: ['*/alarm-test.ts', '*/ticket-dispenser-test.ts'],
 }
--- turn-ticket-dispenser-tests/ticket-dispenser-test.ts
+++ turn-ticket-dispenser-tests/ticket-dispenser-test.ts
@@ -1,17 +1,20 @@
-import { expect } from 'chai';
-import 'mocha';
-import TicketDispenser from '../turn-ticket-dispenser/ticket-dispenser';
+import { expect } from 'chai'
+import 'mocha'
+import TicketDispenser from '../turn-ticket-dispenser/ticket-dispenser'
+import TurnNumberSequence from '../turn-ticket-dispenser/turn-number-sequence'
+import { mock, when } from 'ts-mockito'
+
+// - [ ] Ask for ticket, receive ticket
 
 describe('Turn Ticket Dispenser', () => {
+    it('Ask for ticket, receive ticket with my number', () => {
+        const mockedTurnNumberSequence: typeof TurnNumberSequence =
+            mock(TurnNumberSequence)
 
-	describe('TurnTicketDispenser', () => {
+        when(mockedTurnNumberSequence.getNextTurnNumber()).thenReturn(13)
 
-		it('foo', () => {
-			const dispenser = new TicketDispenser();
-			const ticket = dispenser.getTurnTicket();
-			expect(ticket.getTurnNumber()).to.eql(-1);
-		});
-
-	});
-
-});
+        const dispenser = new TicketDispenser()
+        const ticket = dispenser.getTurnTicket()
+        expect(ticket.getTurnNumber()).to.equal(13)
+    })
+})
