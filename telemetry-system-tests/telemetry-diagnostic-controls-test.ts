5fe0d2d8e810b807e67f0cd5955e253c1ad2a9ca
giuxtaposition <yg97.cs@gmail.com>
2021-10-15 16:13:54 +0200

    added more tests for TelemetryDiagnosticControls

--- telemetry-system-tests/telemetry-diagnostic-controls-test.ts
+++ telemetry-system-tests/telemetry-diagnostic-controls-test.ts
@@ -7,7 +7,7 @@ import {
     TelemetryClientConnectionInterface,
     TelemetryClientDataInterface,
 } from '../telemetry-system/telemetry-client-interface'
-import { instance, mock } from 'ts-mockito'
+import { instance, mock, when } from 'ts-mockito'
 import TelemetryClientData from '../telemetry-system/telemetry-client-data'
 
 // eslint-disable-next-line @typescript-eslint/no-unsafe-argument
@@ -38,6 +38,49 @@ describe('Telemetry System', () => {
                 TelemetryClientData.prototype.diagnosticMessage()
             )
             expect(telemetryClientData.receive).to.have.been.called.once
+        })
+        it('connect and receive correct message', () => {
+            const message = 'message from Client'
+            const telemetryClientData: TelemetryClientDataInterface =
+                mock<TelemetryClientDataInterface>()
+
+            when(telemetryClientData.receive()).thenReturn(message)
+
+            const telemetryClientConnection: TelemetryClientConnectionInterface =
+                mock<TelemetryClientConnectionInterface>()
+
+            when(telemetryClientConnection.getOnlineStatus()).thenReturn(true)
+
+            const telemetryDiagnosticControls = new TelemetryDiagnosticControls(
+                instance(telemetryClientConnection),
+                instance(telemetryClientData)
+            )
+
+            telemetryDiagnosticControls.checkTransmission()
+
+            expect(telemetryDiagnosticControls.readDiagnosticInfo()).to.equal(
+                message
+            )
+        })
+        it('when unable to connect throws error', () => {
+            const telemetryClientData: TelemetryClientDataInterface =
+                new TelemetryClientData()
+
+            const telemetryClientConnection: TelemetryClientConnectionInterface =
+                mock<TelemetryClientConnectionInterface>()
+
+            when(telemetryClientConnection.getOnlineStatus()).thenReturn(false)
+
+            const telemetryDiagnosticControls = new TelemetryDiagnosticControls(
+                instance(telemetryClientConnection),
+                telemetryClientData
+            )
+
+            expect(
+                telemetryDiagnosticControls.checkTransmission.bind(
+                    telemetryDiagnosticControls
+                )
+            ).to.throw('Unable to connect')
         })
     })
 })
