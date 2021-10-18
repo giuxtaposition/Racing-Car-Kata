53696ba9118a71338ef4669b5480235d0a0ba1f3
giuxtaposition <yg97.cs@gmail.com>
2021-10-18 15:19:09 +0200

    added more tests to Leaderboard

--- leaderboard-tests/leaderboard-test.ts
+++ leaderboard-tests/leaderboard-test.ts
@@ -3,11 +3,13 @@ import 'mocha'
 import Driver from '../leaderboard/driver'
 import Leaderboard from '../leaderboard/leaderboard'
 import Race from '../leaderboard/race'
+import SelfDrivingCar from '../leaderboard/self-driving-car'
 
 describe('Leaderboard: ', () => {
     const driver1 = new Driver('Nico Rosberg', 'DE')
     const driver2 = new Driver('Lewis Hamilton', 'UK')
     const driver3 = new Driver('Sebastian Vettel', 'DE')
+    const driver4 = new SelfDrivingCar('1.2', 'Acme')
     const race1 = new Race('Australian Grand Prix', [driver1, driver2, driver3])
     const race2 = new Race('Malaysian Grand Prix', [driver3, driver2, driver1])
     const race3 = new Race('Chinese Grand Prix', [driver2, driver1, driver3])
@@ -24,5 +26,52 @@ describe('Leaderboard: ', () => {
         expect(expectedPoints).to.equal(
             sampleLeaderboard1.driverPoints().get('Lewis Hamilton')
         )
+    })
+
+    it('Leaderboard with self driving cars', () => {
+        const race1 = new Race('Fictional Grand Prix', [
+            driver1,
+            driver2,
+            driver4,
+        ])
+        const race2 = new Race('Fictional Grand Prix', [
+            driver4,
+            driver2,
+            driver1,
+        ])
+        const race3 = new Race('Fictional Grand Prix', [
+            driver2,
+            driver1,
+            driver4,
+        ])
+
+        const sampleLeaderboard = new Leaderboard([race1, race2, race3])
+
+        console.log(sampleLeaderboard.driverRankings())
+
+        expect('Lewis Hamilton').to.equal(sampleLeaderboard.driverRankings()[0])
+    })
+
+    it('Self driving car with different algorithms are considered different cars', () => {
+        const race1 = new Race('Fictional Grand Prix', [
+            driver1,
+            driver2,
+            driver4,
+        ])
+        const race2 = new Race('Fictional Grand Prix', [
+            driver4,
+            driver2,
+            driver1,
+        ])
+        driver4.algorithmVersion = '1.3'
+        const race3 = new Race('Fictional Grand Prix', [
+            driver2,
+            driver1,
+            driver4,
+        ])
+
+        const sampleLeaderboard = new Leaderboard([race1, race2, race3])
+
+        expect(sampleLeaderboard.driverRankings().length).to.equal(4)
     })
 })
